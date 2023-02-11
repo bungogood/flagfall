01555ae3f0fd6c38c746290c416400e5bbf86ca3
Jonathan Good <github@bungogood.com>
2023-02-11 13:48:47 +0000

    Create coordinate system

--- CoreXY/CoreXY.ino
+++ CoreXY/CoreXY.ino
@@ -1,188 +1,188 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <limits.h>
+
+#define MAX_X 600
+#define MAX_Y 540
 
 #define HIGH_SPD 1000 
 #define LOW_SPD  300
 #define CALI_SPD 1500
 
-#define UP         0
-#define DOWN       1
-#define LEFT       2
-#define RIGHT      3
-#define UP_LEFT    4
-#define UP_RIGHT   5
-#define DOWN_LEFT  6
-#define DOWN_RIGHT 7
-
 #define LIMIT_SW_PIN A0
 
+const int fullRev = 6400;
+const int dps = 225;
 
-//  --------
-// |        |
-// |        |
-// M1 ---- M2
+const int default_speed = 1000;
 
+/*
+  +--------+
+  |        |
+  |        |
+  ML ---- MR
+*/
+
+// stored in mm
+typedef struct Vec {
+  int x;
+  int y;
+} Vec;
 
 typedef struct StepperMotor {
-    int DIR_PIN;
-    int STEP_PIN;
-    int DISABLE_PIN;
+  int DIR_PIN;
+  int STEP_PIN;
+  int DISABLE_PIN;
 } StepperMotor;
 
-StepperMotor M1, M2;
+const StepperMotor ML = { DIR_PIN: 4, STEP_PIN: 5, DISABLE_PIN: 12 };
+const StepperMotor MR = { DIR_PIN: 7, STEP_PIN: 6, DISABLE_PIN: 8  };
 
+Vec loc;
 
-void setup()
-{   
-    // Setting motor connection
-    M1.DIR_PIN = 4, M1.STEP_PIN = 5, M1.DISABLE_PIN = 12;
-    M2.DIR_PIN = 7, M2.STEP_PIN = 6, M2.DISABLE_PIN = 8;
+void setup() {
+  // Setting motor pins to output
+  pinMode(ML.DIR_PIN, OUTPUT);
+  pinMode(ML.STEP_PIN, OUTPUT);
+  pinMode(ML.DISABLE_PIN, OUTPUT);
 
-    // Setting motor pins to output
-    pinMode(M1.DIR_PIN, OUTPUT);
-    pinMode(M1.STEP_PIN, OUTPUT);
-    pinMode(M1.DISABLE_PIN, OUTPUT);
+  pinMode(MR.DIR_PIN, OUTPUT);
+  pinMode(MR.STEP_PIN, OUTPUT);
+  pinMode(MR.DISABLE_PIN, OUTPUT);
 
-    pinMode(M2.DIR_PIN, OUTPUT);
-    pinMode(M2.STEP_PIN, OUTPUT);
-    pinMode(M2.DISABLE_PIN, OUTPUT);
+  // Senor
+  pinMode(LIMIT_SW_PIN, INPUT);
 
-    // Senor
-    pinMode(LIMIT_SW_PIN, INPUT);
+  Serial.begin(9600);
 
-    // Enable the motors
-    digitalWrite(M1.DISABLE_PIN, LOW);
-    digitalWrite(M2.DISABLE_PIN, LOW);
+  calibration();
 
-    Serial.begin(9600);
+  go({ MAX_X / 2, MAX_Y / 2 }, 2000);
+  go({ MAX_X, 0 }, 2000);
+  go({ MAX_X / 2, MAX_Y / 2 }, 2000);
+  go({ MAX_X, MAX_Y }, 2000);
+  go({ MAX_X / 2, MAX_Y / 2 }, 2000);
+  go({ 0, MAX_Y }, 2000);
+  go({ MAX_X / 2, MAX_Y / 2 }, 2000);
+  go({ 0, 0 }, 2000);
 }
-void loop()
-{
-    // calibration();
-    // delay(10000);
 
-    move_in_dir(6400 * 2, HIGH_SPD, RIGHT);
-    move_in_dir(6400 * 2, HIGH_SPD, UP_RIGHT);
-    move_in_dir(6400 * 2, HIGH_SPD, UP);
-    move_in_dir(6400 * 2, HIGH_SPD, UP_LEFT);
-    move_in_dir(6400 * 2, HIGH_SPD, LEFT);
-    move_in_dir(6400 * 2, HIGH_SPD, DOWN_LEFT);
-    move_in_dir(6400 * 2, HIGH_SPD, DOWN);
-    move_in_dir(6400 * 2, HIGH_SPD, DOWN_RIGHT);
-    delay(1000);
+void loop() {
 
 }
 
-
+/**
+ * Calibarates the core xy reference fame by moving to Bottom Left (0, 0)
+ */
 void calibration() {
-    /*
-    * Move to the DOWN LEFT corner
-    */
+  // Disable MR so that it is not locked
+  digitalWrite(MR.DISABLE_PIN, HIGH);
 
-    // Disable M2 so that it is not locked
-    digitalWrite(M2.DISABLE_PIN, HIGH);
-    // Enable M1 to rotate
-    digitalWrite(M1.DISABLE_PIN, LOW);
+  // Enable ML to rotate
+  digitalWrite(ML.DISABLE_PIN, LOW);
 
-    int interval = (int) 10000 / CALI_SPD;
+  int interval = (int) 10000 / CALI_SPD;
+  
+  // Set the direction of ML
+  digitalWrite(ML.DIR_PIN, HIGH);
 
-    // Set the direction of M1
-    digitalWrite(M1.DIR_PIN, HIGH);
-
-    // Rotate M1 until hit the switch at A0
-    while (!digitalRead(LIMIT_SW_PIN)) {
-        digitalWrite(M1.STEP_PIN, LOW);
-        delayMicroseconds(interval);
-        digitalWrite(M1.STEP_PIN, HIGH);
-        delayMicroseconds(interval);
-    }
-
-    // Move a bit more to make sure it stays at corner
-    for (int i = 0; i < 500; i++) {
-        digitalWrite(M1.STEP_PIN, LOW);
-        delayMicroseconds(interval);
-        digitalWrite(M1.STEP_PIN, HIGH);
-        delayMicroseconds(interval);
-    }
+  // Rotate ML until hit the switch at A0
+  while (!digitalRead(LIMIT_SW_PIN)) {
+    digitalWrite(ML.STEP_PIN, LOW);
+    delayMicroseconds(interval);
+    digitalWrite(ML.STEP_PIN, HIGH);
+    delayMicroseconds(interval);
+  }
         
-    // Disable M1
-    digitalWrite(M1.DISABLE_PIN, HIGH);
+  // Disable ML
+  digitalWrite(ML.DISABLE_PIN, HIGH);
+
+  // Reset location
+  loc = { 0, 0 };
 }
 
-
-void move_single_motor(StepperMotor motor, int step, int speed, int direction) {
-
-    int interval = (int) 10000 / speed;
-
-    digitalWrite(motor.DIR_PIN, direction);
-
-    // Enable the motor
-    digitalWrite(motor.DISABLE_PIN, LOW);
-    // Making move
-    for (int i = 0; i < step; i++) {
-        digitalWrite(motor.STEP_PIN, LOW);
-        delayMicroseconds(interval);
-        digitalWrite(motor.STEP_PIN, HIGH);
-        delayMicroseconds(interval);
-    }
-    delay(10);
-    // Disable the motor
-    digitalWrite(motor.DISABLE_PIN, HIGH);
+/**
+ * Moves the core xy to a coordinate
+ * Using a default speed
+ *
+ * @param dest is the destination coordinate in mm which the core xy moves to
+ */
+void go(Vec dest) {
+  move({ dest.x - loc.x, dest.y - loc.y }, default_speed);
 }
 
+/**
+ * Moves the core xy to a coordinate
+ *
+ * @param dest is the destination coordinate in mm which the core xy moves to
+ * @param speed is the speed to move to the destination
+ */
+void go(Vec dest, int speed) {
+  if (0 <= dest.x && dest.x <= MAX_X && 0 <= dest.y && dest.y <= MAX_Y) {
+    move({ dest.x - loc.x, dest.y - loc.y }, speed);
+  } else {
+    // invalid coordinate
+  }
+}
 
-// Move 2 motors at the same time
-void move_in_dir(int step, int speed, int direction) {
+/**
+ * Move the core xy by a vector
+ * Using a default speed
+ *
+ * @param diff is a vector which the core xy moves along in mm
+ */
+void move (Vec diff) {
+  move(diff, default_speed);
+}
 
-    int interval = (int) 10000 / speed;
-    
-    // Enable the motor
-    digitalWrite(M1.DISABLE_PIN, LOW);
-    digitalWrite(M2.DISABLE_PIN, LOW);
+/**
+ * Move the core xy by a vector
+ *
+ * @param diff is a vector which the core xy moves along in mm
+ * @param speed is the speed to move along the vector
+ */
+void move(Vec diff, int speed) {
+  int interval = (int) 10000 / speed;
 
-    // Set up 8 moving directions
-    switch (direction) {
-        case UP:
-            digitalWrite(M1.DIR_PIN, LOW);
-            digitalWrite(M2.DIR_PIN, HIGH);
-            break;
-        case DOWN:
-            digitalWrite(M1.DIR_PIN, HIGH);
-            digitalWrite(M2.DIR_PIN, LOW);
-            break;
-        case LEFT:
-            digitalWrite(M1.DIR_PIN, HIGH);
-            digitalWrite(M2.DIR_PIN, HIGH);  
-            break; 
-        case RIGHT:
-            digitalWrite(M1.DIR_PIN, LOW);
-            digitalWrite(M2.DIR_PIN, LOW);
-            break;
+  int dl = (int) (diff.x + diff.y) / 1.414;
+  int dr = (int) (diff.x - diff.y) / 1.414;
 
-        case DOWN_RIGHT:
-            move_single_motor(M2, step, speed, LOW);
-            return 0;
-        case UP_RIGHT:
-            move_single_motor(M1, step, speed, LOW);
-            return 0;
-        case DOWN_LEFT:
-            move_single_motor(M1, step, speed, HIGH);
-            return 0;
-        case UP_LEFT:
-            move_single_motor(M2, step, speed, HIGH);
-            return 0;
+  // Enable the motor
+  digitalWrite(ML.DISABLE_PIN, LOW);
+  digitalWrite(MR.DISABLE_PIN, LOW);
+
+  // directions
+  digitalWrite(ML.DIR_PIN, dl < 0);
+  digitalWrite(MR.DIR_PIN, dr < 0);
+
+  StepperMotor continous, descrete;
+
+  dl = abs(dl); dr = abs(dr);
+  float gradient = min(dl, dr) == 0 ? INT_MAX : max(dl, dr) / min(dl, dr);
+  if (dl > dr) {
+    continous = ML; descrete = MR;
+  } else {
+    continous = MR; descrete = ML;
+  }
+
+  // Making move
+  long steps = (long) dps * max(dl, dr);
+  float counter = 0;
+
+  for (long i = 0; i < steps; i++) {
+    counter++;
+    digitalWrite(continous.STEP_PIN, LOW);
+    if (counter > gradient) digitalWrite(descrete.STEP_PIN, LOW);
+    delayMicroseconds(interval);
+    digitalWrite(continous.STEP_PIN, HIGH);
+    if (counter > gradient) {
+      digitalWrite(descrete.STEP_PIN, HIGH);
+      counter -= gradient;
     }
+    delayMicroseconds(interval);
+  }
 
-    // Making move
-    for (int i = 0; i < step; i++) {
-        digitalWrite(M1.STEP_PIN, LOW);
-        digitalWrite(M2.STEP_PIN, LOW);
-        delayMicroseconds(interval);
-        digitalWrite(M1.STEP_PIN, HIGH);
-        digitalWrite(M2.STEP_PIN, HIGH);
-        delayMicroseconds(interval);
-    }
-
-    // Disable the motor
-    digitalWrite(M1.DISABLE_PIN, HIGH);
-    digitalWrite(M2.DISABLE_PIN, HIGH);
+  // Disable the motor
+  digitalWrite(ML.DISABLE_PIN, HIGH);
+  digitalWrite(MR.DISABLE_PIN, HIGH);
+  loc.x += diff.x; loc.y += diff.y;
 }
