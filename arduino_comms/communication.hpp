a5365105194cfbf47a60770a95ca3f97cc05526a
rubberhead <bjchenzhengyi@hotmail.com>
2023-03-24 13:07:57 +0000

    what

--- arduino_comms/communication.hpp
+++ arduino_comms/communication.hpp
@@ -51,39 +51,7 @@ OpKind get_opkind(const uint8_t* serial_read_buffer) {
     }
 }
 
-/**
- * Perform handshake with Raspi host *once*. 
- * 
- * @return true if handshake is successful. 
- * @return false otherwise. 
- */
-bool handshake() {
-    /* Listen for PC initiation */
-    uint8_t handshake_buf[2] {0}; 
-    // Serial.dtr()
-    // while (!Serial.rts()) continue;
-    Serial.readBytes(handshake_buf, sizeof(handshake_buf)); 
-
-    uint8_t arduino_id; 
-    if (handshake_buf[0] == HANDSHAKE) {
-        // buffer[1..] should be Arduino ID, which is assumed to be non-zero
-        arduino_id = handshake_buf[1]; 
-        Serial.write(handshake_buf, sizeof(handshake_buf)); 
-        Serial.flush();
-        memset(handshake_buf, 0, 2); 
-
-        Serial.readBytes(handshake_buf, 2); 
-
-        if (handshake_buf[0] == HANDSHAKE && handshake_buf[1] == arduino_id) {
-            handshake_buf[0] = ACK; 
-            Serial.write(ACK); 
-            Serial.flush();
-            return true; 
-        } else {
-            return false; 
-        }
-
-    } else {
-        return false; 
-    }
+void write_ack() {
+    uint8_t buf[1] {ACK}; 
+    Serial.write(buf, sizeof(buf)); 
 }
